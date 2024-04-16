3fda7f12d26cc8077e089df0b5cea02af0b9049e
Evan BOISSONNOT <evan@boissonnot.fr>
2024-04-16 17:20:14 +0200

    feat: types prévus par TS

--- apprentissage/decouverte typescript/dist/main.js
+++ apprentissage/decouverte typescript/dist/main.js
@@ -36,7 +36,7 @@ afficherNimp(1);
 //#endregion
 //#region type Pipé
 function afficherStructure(item) {
-    if (typeof item === 'string') {
+    if (typeof item === 'string') { // type guard primitif
         console.info(item.toLowerCase());
     }
     if (typeof item === 'number') {
@@ -57,5 +57,63 @@ var TypeArme;
 const en = 'ARBALETE';
 function affecterTypeArme(type) {
     type = 'Force';
+}
+const armure = {
+    protection: 12,
+    emission: 100
+};
+// Règle :
+/*
+ type : pour le reste (type interne, cadrage de fonction, alias)
+ Interface : DTO (ceux qui viennent de l'api) + implémentation class
+ Class: dès que j'ai besoin d'un constructor => Injection de dépendance
+
+ compléter : généricité <> et le  <extends >
+*/
+//#endregion
+//#region Functions
+function calculerTTC(valueHT) {
+    return valueHT * 1.2;
+}
+function preparerMonnaie(callbackTTC) {
+    const resultTTC = callbackTTC(20);
+}
+function preparerMonnaieBis(callbackTTC) {
+    const resultTTC = callbackTTC('Test'); // Aucun test sur le type
+}
+function preparerMonnaieTer(callbackTTC) {
+    const resultTTC = callbackTTC(20);
+}
+const statistic = {
+    janvier: 10,
+    fevrier: false,
+    mars: false,
+    avril: 12
+};
+const statisticPlus = {
+    janvier: 0,
+    fevrier: 10
+};
+function afficherContent(value) {
+    if (typeof value === 'string') {
+        console.info(value.toLowerCase());
+    }
+}
+function combattre(item) {
+    if ('combattre' in item) {
+        item.combattre();
+    }
+}
+// functional type guard
+function isAvecForce(item) {
+    return item.utiliserForce !== undefined;
+}
+function combattreAmeliore(item) {
+    if (isAvecForce(item)) {
+        item.utiliserForce();
+    }
+    else {
+        item.combattre();
+    }
 }
 //#endregion
--- apprentissage/decouverte typescript/src/main.ts
+++ apprentissage/decouverte typescript/src/main.ts
@@ -1,4 +1,4 @@
-import { Wookiee } from './wookiee'
+// import { Wookiee } from './wookiee'
 import { Gungan } from './gungan'
 import Character from './character'
 import {AvecDeLaVie} from './avec-de-la-vie'
@@ -89,6 +89,28 @@ type TypeArmeAmeliore = TypeArmePipe | 'Force'
 function affecterTypeArme(type: TypeArmeAmeliore): void {
     type = 'Force'
 }
+
+type Armure = {
+    protection: number
+}
+type AvecPouvoirArmure = Armure & {
+    emission: number
+}
+
+const armure: AvecPouvoirArmure = {
+    protection: 12,
+    emission: 100
+}
+
+// Règle :
+/*
+ type : pour le reste (type interne, cadrage de fonction, alias)
+ Interface : DTO (ceux qui viennent de l'api) + implémentation class
+ Class: dès que j'ai besoin d'un constructor => Injection de dépendance
+
+ compléter : généricité <> et le  <extends >
+*/
+
 //#endregion
 
 //#region Functions
@@ -109,3 +131,128 @@ function preparerMonnaieTer(callbackTTC: CalculerTTC): void {
 }
 //#endregion
 
+
+//#region Mapped type
+type BoolOrNumber = {
+    [key: string]: boolean | number
+}
+
+const statistic: BoolOrNumber = {
+    janvier: 10,
+    fevrier: false,
+    mars: false,
+    avril: 12
+}
+
+type Mapped<T> = {
+    [key: string]: T
+}
+const statisticPlus: Mapped<number> = {
+    janvier: 0,
+    fevrier: 10
+}
+//#endregion
+
+//#region Type guard avancé
+type StringOrTableOrNumber = string | string[] | number
+function afficherContent(value: StringOrTableOrNumber) {
+    if (typeof value === 'string') {
+        console.info(value.toLowerCase())
+    }
+}
+
+type MethodeVide = () => void
+
+type Droide = { combattre: MethodeVide }
+type Jedi = { utiliserForce: MethodeVide }
+
+function combattre(item: Droide | Jedi) {
+    if ('combattre' in item) {
+        item.combattre()
+    }
+}
+
+// functional type guard
+function isAvecForce(item: Droide | Jedi): item is Jedi {
+    return (item as Jedi).utiliserForce !== undefined
+}
+
+function combattreAmeliore(item: Droide | Jedi) {
+    if(isAvecForce(item)) {
+        item.utiliserForce()
+    } else {
+        item.combattre()
+    }
+}
+//#endregion
+
+//#region Record
+type ListeMois = 'janvier' | 'fevrier' | 'mars'
+type Stat = {
+    date: Date,
+    nbParties: number
+}
+const statList: Record<ListeMois, Stat> = {
+    janvier: { date: new Date(), nbParties: 10 },
+    fevrier: { date: new Date(), nbParties: 1 },
+    mars: { date: new Date(), nbParties: 20 }
+}
+//#endregion
+
+//#region Types supplémentaires
+interface Wookiee {
+    id: number,
+    prenom: string,
+    age: number,
+    height: number
+}
+
+type PickWookiee = Pick<Wookiee, 'prenom' | 'age'>
+function updateWookieeStep2(wookie: PickWookiee): void {
+    wookie.age = 12
+    wookie.prenom = 'chewie'
+}
+
+type OmitWookiee = Omit<Wookiee, 'id'>
+function createWookiee(wookie: OmitWookiee): void {
+    wookie.age = 12
+    wookie.height = 250
+    wookie.prenom = 'chewie'
+}
+
+type WookieeComplet = OmitWookiee &  {
+    id: number
+}
+
+type PartialWookiee = Partial<Wookiee>
+const partial: PartialWookiee = {
+    id: 10,
+    prenom: 'Chewie'
+}
+
+type StepOneWookiee = Pick<PartialWookiee, 'prenom' | 'age'>
+type StepTwoWookiee = Pick<PartialWookiee, 'height'>
+
+const stepOne: StepOneWookiee = {
+    age: 18,
+    prenom: 'Chewie'
+}
+
+const stepTwo: StepTwoWookiee = {
+    height: 250
+}
+
+const final: PartialWookiee = {
+    ...stepOne,
+    ...stepTwo
+}
+
+type RequiredWookiee = Required<Pick<PartialWookiee, 'prenom'>> 
+
+
+type GameConfig = {
+    nbCol: number,
+    nbRows: number
+}
+type ReadonlyConfig = Readonly<GameConfig>
+//#endregion
